399ec9ac16d212dcb226ca5f2e1e918b825c9f4b
Jason Williams <jasonwilliams@Jasons-MacBook-Pro.local>
2019-02-26 21:04:47 -0500

    add fuzzy matching to dictionary input

--- dictionary/mydictionary.py
+++ dictionary/mydictionary.py
@@ -1,16 +1,22 @@
 #first, load the json data into a dictionary
 
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def getDefinition (keyword):
+
+	# Account for case in intput
 	keyword = keyword.lower()
 	if keyword in data:
 		return data[keyword]
+	#check to see if you can make a fuzzy match of the term
+	elif len( get_close_matches(keyword, data.keys(),cutoff=0.8)) > 0:
+		return "That word is not in the dictionary. Did you mean: %s?" % get_close_matches(keyword, data.keys(),cutoff=0.8)[0]
 	else:
 		return "This word is not in the dictionary. Please double check it."
 
 
-# test ability to return keyword
+# Take input and return definition for entered term
 keyword = input("Enter a word:")
 print(getDefinition(keyword))
